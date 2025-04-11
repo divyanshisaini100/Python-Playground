10fdd6950281b77891de616f5066d4fd7580ad76
divyanshisaini100 <divyanshisaini100@gmail.com>
2025-04-11 08:32:37 +0000

    ubi-dubi

--- ubi-dubi/README.md
+++ ubi-dubi/README.md
@@ -1,4 +1,4 @@
-# Ubi-dubi
+# Ub-dub
 there is a text file named "paragraph.txt" 
-modify the file such that before each vowel, "ubi" and "dubi" is added alternatively
-Say for "hello world" text, the output should be "h**ubi**ell**dubi**o w**ubi**orld"
+modify the file such that before each vowel, "ub" and "dub" is added alternatively
+Say for "hello world" text, the output should be "h**ub**ell**dub**o w**ub**orld"
--- ubi-dubi/ubidubi.py
+++ ubi-dubi/ubidubi.py
@@ -1,14 +1,20 @@
-with open("paragraph.txt", 'r' ) as f:
-    
-    text = f.read()
-    
+with open("paragraph.txt", 'r' ) as f:     
+      
+     #'r' is default
+    # text = f.read() ....makes it into a single long string and then you directly go 'for char in text: '
+    # for line in f ....reads it line by line and then you need 'for char in line: '
+      
     vowel_count = 0
-    for line in text:
+    for line in f:
         for char in line:
-            if char.lowercase() in 'aeiou':
-                if vowel_count/2 == 0:
-                    print('ubi',end='') 
+            if char.lower() in 'aeiou':
+                if vowel_count % 2 == 0:
+                    print('ub',end='') 
                 else:
-                    print('dubi',end='')
+                    print('dub',end='')
                 vowel_count+=1    
             print(char, end='')
+
+
+# hubelldubo wuborld
+# pythdubon ubis gduboubod            
