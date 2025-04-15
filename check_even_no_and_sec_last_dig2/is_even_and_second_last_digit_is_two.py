c8422fa141a4bee142d70cdeaf8a10da8c8025da
divyanshisaini100 <divyanshisaini100@gmail.com>
2025-04-15 18:33:22 +0000

    check_amended

--- check_even_no_and_sec_last_dig2/is_even_and_second_last_digit_is_two.py
+++ check_even_no_and_sec_last_dig2/is_even_and_second_last_digit_is_two.py
@@ -1,2 +1,5 @@
 def is_even_and_second_last_digit_is_two(n):
-    return n%2 == 0 and str(n)[-2] == 2
+    return n%2 == 0 and str(n)[-2] == "2"     # NOTE: 2 must come in quotes because comparing with str
+
+n = int(input())
+print(is_even_and_second_last_digit_is_two(n))
