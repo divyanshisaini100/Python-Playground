85e63bc3893b7dfb07aec505a9092b501c98f8b6
divyanshisaini100 <divyanshisaini100@gmail.com>
2025-04-16 02:20:59 +0000

    minor_Note

--- check_even_no_and_sec_last_dig2/is_even_and_second_last_digit_is_two.py
+++ check_even_no_and_sec_last_dig2/is_even_and_second_last_digit_is_two.py
@@ -1,5 +1,8 @@
 def is_even_and_second_last_digit_is_two(n):
     return n%2 == 0 and str(n)[-2] == "2"     # NOTE: 2 must come in quotes because comparing with str
 
+# or to check secondlast digit = 2.....abs(num)//10%10 ==2
+# we added %10  above incase input integer is 3 digits or more
+
 n = int(input())
 print(is_even_and_second_last_digit_is_two(n))
