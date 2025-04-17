e6198ec9aeeae9995733245be2d1b6bf03d1d3ba
divyanshisaini100 <divyanshisaini100@gmail.com>
2025-04-17 17:54:02 +0000

    max_col_sum_&_index

---
+++ Max_col_sum_&_col_sum_index/main.py
@@ -0,0 +1,13 @@
+m,n = map(int,input().split())
+rows = []
+
+for _ in range(m):
+    rows.append(list(map(int, input().split())))
+
+def col_sum(rows, c):
+    return sum(rows[i][c] for i in range(m))  
+
+col_sums_max = max(col_sum(rows, c) for c in range(n))
+max_col_sum_index = next(c for c in range(n) if col_sum(rows,c)==col_sums_max)  #next(...) works with the generator expression and returns the first match....if we had put [] then it would  have returned all the indices and with max match and not just first one  
+print(f'max col sum is {col_sums_max}')
+print(f'max col sum index is {max_col_sum_index}')
