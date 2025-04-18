591a6dbb554fedc230b3233733db221b51db9c8c
divyanshisaini100 <divyanshisaini100@gmail.com>
2025-04-18 15:42:47 +0000

    Diamond_print

--- Diamond_print /print.py
+++ Diamond_print /print.py
@@ -7,3 +7,9 @@ for i in range(n-1,0,-1):
     print(" " * (n-i) + "*" * (2*i-1))
 
 
+# using f-string padding
+
+for i in range(1, n+1):
+    print(f'{"*" * (2*i - 1):^{m}}') 
+for i in range(n-1,0,-1) :
+    print(f'{"*" * (2*i - 1):^{m}}')  
