4e3f4e378503a73b64953b2db3a056d6ea61cfd3
divyanshisaini100 <divyanshisaini100@gmail.com>
2025-04-22 17:48:47 +0000

    init commit

--- is_positive_odd_or_negative_even.py
+++ is_positive_odd_or_negative_even.py
@@ -1,5 +1,6 @@
 def is_positive_odd_or_negative_even(n):
     return (n%2 != 0 and n == abs(n)) or (n%2 == 0 and n != abs(n))
-
+    #return (n % 2 != 0 and n > 0) or (n % 2 == 0 and n < 0)     MORE EFFICIENT
+    
 n = int(input())
 print(is_positive_odd_or_negative_even(n))
