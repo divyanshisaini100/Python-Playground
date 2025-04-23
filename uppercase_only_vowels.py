45e9c7099f1fa8611691964928ceea8e429fb7fa
divyanshisaini100 <divyanshisaini100@gmail.com>
2025-04-23 18:33:42 +0000

    minor change

--- uppercase_only_vowels.py
+++ uppercase_only_vowels.py
@@ -5,8 +5,11 @@ for i in range(n):
       if char in 'aeiou':
          char = char.upper()
       print (char,end ="")
+   print()   #to move to next line after every line 
 
-for i in range(n):
-    line = input()
-    print("".join([c.upper() if c.lower() in "aeiou" else c.lower() for c in line]))
+#NOTE: Below is more efficient code :-     
+
+# for i in range(n):
+#     line = input()
+#     print("".join([c.upper() if c.lower() in "aeiou" else c.lower() for c in line]))
 
