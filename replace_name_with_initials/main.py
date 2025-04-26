2f11b42da3bbcbd4a2d6ea8abc9fbde42a528314
divyanshisaini100 <divyanshisaini100@gmail.com>
2025-04-26 15:55:49 +0000

    small_NOTICE:)

--- replace_name_with_initials/main.py
+++ replace_name_with_initials/main.py
@@ -6,7 +6,9 @@ for _ in range(n):
 
 for name in sorted(names):
     parts = name.split()    
-    print(f" {". ".join(map(lambda x: x[0], parts[:-1]))}. {parts[-1]}" )
+    print(f" {". ".join(map(lambda x: x[0], parts[:-1]))}. {parts[-1]}" ) 
+    
+# NOTICE even before printing parts[-1],  we gotta include ". " because join() functions works only in between (NOT AFTER END ONE, LIKE  WE WANT HERE) !!! 
 
    
 #YAAAAAAYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY! HUAAA HURRAYYY!!!!
