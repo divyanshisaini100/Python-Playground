35df0127211a3adbe6d293e713ccd667c795fddd
divyanshisaini100 <divyanshisaini100@gmail.com>
2025-05-06 16:10:22 +0000

    xprint

--- xprint.py
+++ xprint.py
@@ -20,5 +20,20 @@ for i in range(1,n//2):
 print(' '*(n//2) + 'x') 
 for i in range(1,n//2):
     print((i-1)*' ' + '/' + ' '*(5-i*2)+'\\') 
-     
+
+
+    #USING ONLY SLASHES AND SPACES     
+for i in range(n):
+    for j in range(n):
+        if i == j and j == n // 2:
+            print("x", end="")  # center
+        elif j == i:
+            print("\\", end="")
+        elif j == n - i - 1:
+            print("/", end="")
+        else:
+            print(" ", end="")
+    print()
+
+
 
